d0ef5b0fe7cbf422d3e2974e00f6de55836fa4d3
Shrikant Paliwal <shrikantpaliwal001@gmail.com>
2023-02-11 16:50:01 +0530

    Definately a fix

---
+++ GoogleSearch/src/components/Results.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+
+import { useStateContext } from '../context/StateContextProvider';
+import { Loading } from './Loading';
+
+const Results = () => {
+  return (
+    <div>
+      <h1>Results</h1>
+    </div>
+  )
+}
+
+export default Results
--- GoogleSearch/src/main.jsx
+++ GoogleSearch/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { StateContextProvider } from './context/StateContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StateContextProvider>
+    <React.StrictMode>
+      <Router><App /></Router>
+    </React.StrictMode>
+  </StateContextProvider>
 )
